3b6397eb17e73daaefd96e0a30e9fb2e995ab2ec
Shamim <shamimarifin906@gmail.com>
2022-12-15 15:06:26 +0600

    add file extention on gitiginore

---
+++ imageZoom/script.js
@@ -0,0 +1,16 @@
+
+
+let imgSet = document.querySelector('#imageSet');
+
+
+function changeOne(){
+    imgSet.src = '2.jpg'
+}
+
+function changeTwo(){
+    imgSet.src = '3.jpg'
+}
+
+function changeThree(){
+    imgSet.src = '4.jpg'
+}
