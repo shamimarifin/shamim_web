293a80ea9cd7a1181480dd19102ebe4139a1b7f5
Shamim <shamimarifin906@gmail.com>
2022-12-16 11:46:17 +0600

    responsive add file

---
+++ responsiveMenu/script.js
@@ -0,0 +1,11 @@
+ let toggle = document.querySelector('#icone');
+ let main = document.querySelector('.main_menu');
+ 
+ toggle.addEventListener('click' , function(){
+
+
+
+    main.classList.toggle('show');
+
+
+ })
